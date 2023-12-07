1b6c9b1e8d5c5e88c7b338ccf261b73aabaca04a
sophearyrin1 <srin@miu.edu>
2023-12-07 10:55:48 -0600

    js operand

--- fundamental/hello.js
+++ fundamental/hello.js
@@ -1,4 +1,4 @@
-alert('Hello worldworld!');
+// alert('Hello worldworld!');
 let name = "Anna";
 //using batis
 console.log(`My name is  ${name}`);
@@ -9,3 +9,5 @@ console.log(`My name is ` + name);
 // console.log(address); // output: undefined
 console.log(typeof name);
 console.log(typeof Symbol("id"));
+console.log(+1);
+console.log(+true); //output: 1
